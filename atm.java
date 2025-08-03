3cd70b4c067d213987e28fc204a62c3257c0ab96
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2025-08-03 12:35:55 +0530

    Update atm.java

--- atm.java
+++ atm.java
@@ -1,34 +1,35 @@
 //WRITING THIS CODE AS PRACTISE TO MAKE A ATM MACHINE
 import java.util.Scanner;
+
 public class atm {
     public static void main (String[] args){
         Scanner scanner = new Scanner(System.in);
-        int bal=0;
+        double balance=0;
         System.out.println("THIS IS A ATM MACHINE");
         System.out.println("choose 1.withdraw");
         System.out.println("choose 2.deposit");
         System.out.println("choose 3.exit");
-        int n= Scanner.nextInt();
+        int n= scanner.nextInt();
 
         switch (n) {
-            case 1->withdraw();
-            case 2->deposit();
+            case 1 ->System.out.println( withdraw(balance));
+            case 2 ->System.out.println( deposit(balance));
     }
     
 
 }
-    static int withdraw(int amt){
+    static double withdraw(double balance){
         System.out.println("how much amout do u wanna take out");
-        amt = Scanner.nextInt();
+        double amt = Scanner.nextInt();
         System.out.println("now available balance is:");
         return (bal-amt);
     }
 
-    static int deposit(int amt2){
+    static double  deposit(double balance){
         System.out.println("how much amount do you wanna deposit");
-        amt2=Scanner.nextInt();
+        double amt2=Scanner.nextInt();
         System.out.println("now available balance is:");
-        return (bal+deposit);
+        return (bal+amt2);
     }
     
 }
