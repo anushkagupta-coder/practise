7062866eee01f202cd967dc8e60f57f71ab8189a
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2025-08-04 23:21:06 +0530

    Update studentgrade.java

--- studentgrade.java
+++ studentgrade.java
@@ -4,13 +4,16 @@ import java.util.Scanner;
 public class studentgrade{
     static Scanner scanner = new Scanner(System.in);
     public static void main (String[] args){
-        int marks;
-        int avg;
-        int total;
+        double[] marks=new marks[3];
+        double avg;
+        double total;
         char grade;
-        Straing name;
+        String name;
     }
     static void get(){
+        System.out.println("enter name of student");
+        name=scanner.nextLine();
+        System.out.println("enter marks of 3 subjects");
 
     }
     static void calculate(){
