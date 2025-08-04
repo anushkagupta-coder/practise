96fab1d18e183c3f01978d916d72d80bce3afcc1
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2025-08-04 23:19:14 +0530

    Update studentgrade.java

--- studentgrade.java
+++ studentgrade.java
@@ -1,6 +1,22 @@
+
+import java.util.Scanner;
+
 public class studentgrade{
+    static Scanner scanner = new Scanner(System.in);
     public static void main (String[] args){
+        int marks;
+        int avg;
+        int total;
+        char grade;
+        Straing name;
+    }
+    static void get(){
 
     }
-    
+    static void calculate(){
+
+    }
+    static void display(){
+
+    }
 }
